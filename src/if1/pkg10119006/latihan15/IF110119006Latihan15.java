b68c13fe2153920c3bde7a8fcb1848759bc9be06
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 10:23:16 +0700

    fix

---
+++ src/if1/pkg10119006/latihan15/IF110119006Latihan15.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan15;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil Logika
+ */
+public class IF110119006Latihan15 {
+
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = false;
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("!a = " + !a);
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
